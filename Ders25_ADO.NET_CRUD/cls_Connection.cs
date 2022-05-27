66548673358afa1f5e473d0c0a3e50b56217b355
Erensu Deveci <erensudeveci@gmail.com>
2022-05-27 12:13:16 +0300

    Add project files.

---
+++ Ders25_ADO.NET_CRUD/cls_Connection.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ders25_ADO.NET_CRUD
+{
+    public class cls_Connection
+    {
+        public static SqlConnection baglanti
+        {
+            get
+            {
+                SqlConnection sqlcon = new SqlConnection("Server=MACHINEX;Database=NORTHWND;Trusted_Connection=True");
+                return sqlcon;
+            }
+        }
+    }
+}
